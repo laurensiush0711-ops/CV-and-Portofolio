a87d89af8bd6eadbeefa02f46d18d2bd233309df
laurensiush0711-ops <laurensiush.0711@gmail.com>
2026-02-16 14:22:27 +0700

    Add files via upload

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,5 @@
-export {};
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      readonly API_KEY: string;
-      [key: string]: string | undefined;
-    }
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly API_KEY: string;
   }
 }
